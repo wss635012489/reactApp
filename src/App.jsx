6ef113a4e0e5e532b5e524dcd89b09e0ee1412da
王双双 <635012489@qq.com>
2022-02-24 10:28:14 +0800

    11111

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Home from "./home/Home"
+import "./App.css"
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <Home></Home>
+      </div>
+    ) 
+  }
+}
